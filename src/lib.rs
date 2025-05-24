4096085d6bef6f4b74cd9d171bb4a7c194b7e0fe
Mark Friedenbach <mark@friedenbach.org>
2025-05-24 11:10:52 -0700

    License under the terms of the MPL-2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file,
+// You can obtain one at <https://mozilla.org/MPL/2.0/>.
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -12,3 +16,5 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+// End of File
